7e70d25678b5bf80140f7891944ea35c45e9c5a5
LickevicVL <valery.lickevich@folium.cloud>
2018-05-20 16:25:59 +0300

    Add main

---
+++ main.py
@@ -0,0 +1,47 @@
+import json
+import urequests
+import utime
+from random import randint
+
+
+def main(chip_id, host):
+    velocity = [0, 0]
+    data = [randint(-100, 100) / 100, randint(-100, 100) / 100]
+    iteration = 0
+    url = host + 'data'
+    while iteration != 20:
+        print('Data: ', data)
+        response = urequests.post(url, data=json.dumps({
+            'id': chip_id,
+            'iteration': iteration,
+            'data': data
+        }))
+        print(response.json(), response.status_code)
+        while True:
+            response = urequests.get(url)
+            print(response.json(), response.status_code)
+            if response.status_code == 200:
+                break
+
+            utime.sleep_ms(5000)
+
+        pbest = response.json()['Pbest']
+        gbest = response.json()['Gbest']
+
+        for i in range(2):
+            r1 = randint(0, 100) / 100
+            r2 = randint(0, 100) / 100
+            velocity[i] = 0.5 * velocity[i] + 1 * r1 * (pbest[i] - data[i]) + 1 * r2 * (gbest[i] - data[i])
+            data[i] += velocity[i]
+
+        iteration += 1
+
+    print('BY')
+
+
+if __name__ == '__main__':
+    import urandom
+
+    host = 'http://192.168.1.103:8888/'
+    chip_id = str(urandom.getrandbits(30))
+    main(chip_id, host)
