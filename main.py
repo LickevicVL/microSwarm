29bcbb0ca67e8819eeba7fd373d4166256dbe9c1
LickevicVL <valery.lickevich@folium.cloud>
2018-05-20 23:43:57 +0300

    Add some changes (#2)

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import json
+from random import randint
+
 import urequests
 import utime
-from random import randint
 
 
 def main(chip_id, host):
     velocity = [0, 0]
-    data = [randint(-100, 100) / 100, randint(-100, 100) / 100]
+    v1 = int(str(utime.time()).split('.')[1])
+    v2 = int(str(utime.time()).split('.')[1])
+    d1 = v1 / 1000000 * (-1)**(v1 % 2)
+    d2 = v2 / 1000000 * (-1)**(v1 % 2)
+    data = [d1, d2]
     iteration = 0
     url = host + 'data'
     while iteration != 20:
@@ -33,7 +38,8 @@ def main(chip_id, host):
         for i in range(2):
             r1 = randint(0, 100) / 100
             r2 = randint(0, 100) / 100
-            velocity[i] = 0.5 * velocity[i] + 1 * r1 * (pbest[i] - data[i]) + 1 * r2 * (gbest[i] - data[i])
+            velocity[i] = 0.5 * velocity[i] + 1 * r1 * (
+                pbest[i] - data[i]) + 1 * r2 * (gbest[i] - data[i])
             data[i] += velocity[i]
 
         iteration += 1
@@ -42,8 +48,6 @@ def main(chip_id, host):
 
 
 if __name__ == '__main__':
-    import urandom
-
     host = 'http://192.168.1.103:8888/'
-    chip_id = str(urandom.getrandbits(30))
+    chip_id = str(utime.time()).split('.')[1]
     main(chip_id, host)
